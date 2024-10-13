c0569ba23712b4b678feee8c1f6875bbef68f421
Matthew Buchowiec <matthew.m.buchowiec@vanderbilt.edu>
2024-10-13 10:44:52 -0500

    rewrite

--- metal-renderer/main.cpp
+++ metal-renderer/main.cpp
@@ -5,10 +5,81 @@
 //  Created by Matthew Buchowiec on 10/12/24.
 //
 
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_metal.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("metal-renderer",
+                                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          SCREEN_WIDTH, SCREEN_HEIGHT,
+                                          SDL_WINDOW_SHOWN | SDL_WINDOW_METAL);
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_MetalView metalView = SDL_Metal_CreateView(window);
+    CA::MetalLayer* metalLayer = (CA::MetalLayer*)SDL_Metal_GetLayer(metalView);
+
+    MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    metalLayer->setDevice(device);
+    metalLayer->setPixelFormat(MTL::PixelFormat::PixelFormatBGRA8Unorm);
+
+    MTL::CommandQueue* commandQueue = device->newCommandQueue();
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        // Get the next drawable
+        CA::MetalDrawable* drawable = metalLayer->nextDrawable();
+
+        // Create a render pass descriptor
+        MTL::RenderPassDescriptor* renderPassDescriptor = MTL::RenderPassDescriptor::alloc()->init();
+        renderPassDescriptor->colorAttachments()->object(0)->setTexture(drawable->texture());
+        renderPassDescriptor->colorAttachments()->object(0)->setLoadAction(MTL::LoadAction::LoadActionClear);
+        renderPassDescriptor->colorAttachments()->object(0)->setClearColor(MTL::ClearColor::Make(0.0, 0.5, 1.0, 1.0)); // Blue color
+
+        // Create a command buffer and encoder
+        MTL::CommandBuffer* commandBuffer = commandQueue->commandBuffer();
+        MTL::RenderCommandEncoder* renderEncoder = commandBuffer->renderCommandEncoder(renderPassDescriptor);
+
+        // End encoding and commit
+        renderEncoder->endEncoding();
+        commandBuffer->presentDrawable(drawable);
+        commandBuffer->commit();
+
+        // Release resources
+        renderPassDescriptor->release();
+        renderEncoder->release();
+    }
+
+    // Cleanup
+    commandQueue->release();
+    device->release();
+    SDL_Metal_DestroyView(metalView);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
